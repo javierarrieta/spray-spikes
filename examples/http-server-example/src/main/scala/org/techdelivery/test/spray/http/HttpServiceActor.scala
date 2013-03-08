79b91ff66c8378676490685fc59e78aad5442324
jarrieta <jarrieta@newbay.com>
2013-03-08 17:33:14 +0000

    receiving bytes in chunked requests

--- examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/HttpServiceActor.scala
+++ examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/HttpServiceActor.scala
@@ -3,7 +3,6 @@ package org.techdelivery.test.spray.http
 import akka.actor.Actor
 import spray.util.SprayActorLogging
 import spray.http.HttpRequest
-import spray.http.HttpResponse
 import spray.http.HttpMethods._
 import akka.actor.actorRef2Scala
 import spray.http.HttpEntity.apply
@@ -11,27 +10,27 @@ import spray.http.ChunkedRequestStart
 import spray.http.MessageChunk
 import spray.http.ChunkedMessageEnd
 import spray.http.HttpResponse
+import java.util.UUID
 
 class HttpServiceActor extends Actor with SprayActorLogging {
-
+  val uuid:String = UUID.randomUUID().toString
   def receive = {
     case HttpRequest(GET, "/ping", _, _, _) =>
       sender ! HttpResponse(entity = "pong")
       
     case HttpRequest(POST, "/file",headers,body,_) =>
-      val fileBody = body
-      val fileHeaders = headers
+      log.info( "File uploaded " + body.buffer.length + " bytes :"+ headers )
+
       sender ! HttpResponse( status = 201)
       
     case ChunkedRequestStart(request) =>
-      val req = request
+      log.info( "Chunked request start, headers: " + request.headers )
       
-    case MessageChunk(body,_) =>
-      val entity = body
+    case MessageChunk(body,extensions) =>
+      log.info( "Chunk received: " + body.length + " bytes, extensions: " + extensions )
     
     case ChunkedMessageEnd(extensions, trailer) =>
-      val ext = extensions
-      val headers = trailer
+      log.info( "Chunked request end, headers: " + trailer )
       
       sender ! HttpResponse(status = 201)
       

9bce6f6abb0f81398bb6baf4bb864ea90056296e
javier <javier@techdelivery.es>
2013-03-09 20:03:21 +0000

    Writing to file

--- examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/Boot.scala
+++ examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/Boot.scala
@@ -2,12 +2,24 @@ package org.techdelivery.test.spray.http
 
 import spray.can.server.SprayCanHttpServerApp
 import akka.actor.Props
+import spray.can.server.HttpServer
+import spray.io.PerConnectionHandler
+import spray.io.IOExtension
+import spray.io.PipelineContext
+import akka.actor.ActorRef
+import spray.io.PerConnectionHandler
+import spray.can.server.ServerSettings
+import java.util.UUID
 
 object Boot extends App with SprayCanHttpServerApp {
   
-  // create and start our service actor
-  val service = system.actorOf(Props[HttpServiceActor], "http-service")
-
+  def messageCreator(ctx:PipelineContext) : ActorRef = {
+    val actorId: String = UUID.randomUUID().toString()
+    system.actorOf(Props[HttpServiceActor], actorId)
+  }
+  
+  val ioBridge = IOExtension(system).ioBridge()
+  val httpServer = system.actorOf(Props(new HttpServer(ioBridge, PerConnectionHandler(messageCreator), ServerSettings())), "http-server")
   // create a new HttpServer using our handler and tell it where to bind to
-  newHttpServer(service) ! Bind(interface = "localhost", port = 9080)
+  httpServer ! Bind(interface = "localhost", port = 9080)
 }
--- examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/HttpServiceActor.scala
+++ examples/http-server-example/src/main/scala/org/techdelivery/test/spray/http/HttpServiceActor.scala
@@ -11,29 +11,45 @@ import spray.http.MessageChunk
 import spray.http.ChunkedMessageEnd
 import spray.http.HttpResponse
 import java.util.UUID
+import akka.actor.ActorRef
+import spray.http.Timeout
+import java.io.File
+import java.io.FileOutputStream
 
 class HttpServiceActor extends Actor with SprayActorLogging {
-  val uuid:String = UUID.randomUUID().toString
+  val uuid = UUID.randomUUID().toString()
+  val f: File = new File("/tmp",uuid + ".tmp")
+  val fs = new FileOutputStream(f)
   def receive = {
     case HttpRequest(GET, "/ping", _, _, _) =>
       sender ! HttpResponse(entity = "pong")
       
     case HttpRequest(POST, "/file",headers,body,_) =>
-      log.info( "File uploaded " + body.buffer.length + " bytes :"+ headers )
+      //log.info( "File uploaded " + body.buffer.length + " bytes :"+ uuid )
 
       sender ! HttpResponse( status = 201)
       
     case ChunkedRequestStart(request) =>
-      log.info( "Chunked request start, headers: " + request.headers )
+      //log.info( "Chunked request start[" + uuid + "], headers: " + request.headers )
+      val ref = sender
+      log.info("Starting write to file " + f.getCanonicalPath() )
       
     case MessageChunk(body,extensions) =>
-      log.info( "Chunk received: " + body.length + " bytes, extensions: " + extensions )
+      log.info( "Chunk received: " + body.length + " bytes, to file: " + f.getCanonicalPath() )
+      val ref = sender
+      fs.write(body.buffer)
+//      Thread.sleep(20)
+//      sender ! HttpResponse(status=100)
     
     case ChunkedMessageEnd(extensions, trailer) =>
-      log.info( "Chunked request end, headers: " + trailer )
-      
+      log.info( "Chunked request end, file: " + f.getCanonicalPath() )
+      fs.close()      
       sender ! HttpResponse(status = 201)
       
-    case _: HttpRequest => sender ! HttpResponse(status = 404, entity = "Unknown resource!")
+//    case Timeout(request) =>
+//      sender ! HttpResponse(status = 502, entity = "Timeout processing request: " + uuid)
+      
+    case _: HttpRequest => 
+      sender ! HttpResponse(status = 404, entity = "Unknown resource!")
   }
 }
